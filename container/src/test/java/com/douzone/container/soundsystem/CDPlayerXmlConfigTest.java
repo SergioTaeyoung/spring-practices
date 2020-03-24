8467f0d58ceb9bad1d9ff0c3c8a069f4bd2bfda1
R35 <R35@DESKTOP-O3I6T17>
2020-03-24 12:10:05 +0900

    XML Config #2

--- container/src/test/java/com/douzone/container/soundsystem/CDPlayerXmlConfigTest.java
+++ container/src/test/java/com/douzone/container/soundsystem/CDPlayerXmlConfigTest.java
@@ -11,26 +11,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import config.soundsystem.CDPlayerConfig;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes=CDPlayerConfig.class)
+@ContextConfiguration(locations= {"classpath:config/soundsystem/CDPlayerConfig.xml"})
 public class CDPlayerXmlConfigTest {
 	@Rule
 	public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
-
+	
 	@Autowired
 	private CDPlayer cdPlayer;
-
+	
 	@Test
 	public void testCDPlayerNull() {
 		assertNotNull(cdPlayer);
 	}
-
+	
 	@Test
 	public void testPlay() {
 		cdPlayer.play();
 		assertEquals("Playing 지구멸망 by 양승호", systemOutRule.getLog().replace("\r\n", "").replace("\n", ""));
 	}
-
 }
